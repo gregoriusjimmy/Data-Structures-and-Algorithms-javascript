8b05d13ef59ab9eae951958d2bf06a8f8eae3743
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2021-01-21 19:54:43 +0700

    fix selection sort

--- Algorithms/SelectionSort/selectionSort.js
+++ Algorithms/SelectionSort/selectionSort.js
@@ -1,39 +1,36 @@
 function selectionSort(arr) {
-  let minimumIndex = 0;
-  let needToSwap = false;
+  const swap = (arr, idx1, idx2) => ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]])
+
   for (let i = 0; i < arr.length; i++) {
+    let lowest = i
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minimumIndex]) {
-        minimumIndex = j;
-        needToSwap = true;
-      }
-      if (j == arr.length - 1) {
-        if (needToSwap) {
-          [arr[i], arr[minimumIndex]] = [arr[minimumIndex], arr[i]];
-        }
+      if (arr[lowest] > arr[j]) {
+        lowest = j
       }
     }
+    if (i !== lowest) swap(arr, i, lowest)
   }
-  return arr;
+
+  return arr
 }
 function selectionSort(arr, comparator) {
   if (typeof comparator !== 'function') {
     comparator = (a, b) => {
-      return a - b;
-    };
+      return a - b
+    }
   }
 
   for (let i = 0; i < arr.length; i++) {
-    let minimumIndex = i;
+    let minimumIndex = i
     for (let j = i + 1; j < arr.length; j++) {
-      console.log(i, j);
-      if (comparator(arr[j], arr[minimumIndex]) < 0) minimumIndex = j;
+      console.log(i, j)
+      if (comparator(arr[j], arr[minimumIndex]) < 0) minimumIndex = j
     }
     if (i !== minimumIndex) {
-      [arr[i], arr[minimumIndex]] = [arr[minimumIndex], arr[i]];
+      ;[arr[i], arr[minimumIndex]] = [arr[minimumIndex], arr[i]]
     }
   }
-  return arr;
+  return arr
 }
 
 let keets = [
@@ -53,10 +50,10 @@ let keets = [
     name: 'aa',
     age: 40,
   },
-];
+]
 function test(a, b) {
-  return a.age - b.age;
+  return a.age - b.age
 }
 
 // selectionSort([2, 4, 1,24,52,12,65, 2, 3, 5, 6, 3],test)
-selectionSort(keets, test);
+selectionSort(keets, test)
